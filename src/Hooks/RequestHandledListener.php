579749d2670b7baa0951cf2056f024009bcb1a25
Josue Gomes <josuerodrigovieira@gmail.com>
2026-09-14 09:25:16 -0300

    feat: ignore 404 telemetry on request handled

--- src/Hooks/RequestHandledListener.php
+++ src/Hooks/RequestHandledListener.php
@@ -3,6 +3,7 @@
 namespace Tyto\Agent\Hooks;
 
 use Illuminate\Foundation\Http\Events\RequestHandled;
+use Symfony\Component\HttpFoundation\Response;
 use Throwable;
 use Tyto\Agent\Core;
 use Tyto\Agent\ExecutionStage;
@@ -25,6 +26,13 @@ final class RequestHandledListener
     public function __invoke(RequestHandled $event): void
     {
         try {
+            if ($event->response->getStatusCode() === Response::HTTP_NOT_FOUND && (bool) config('tyto.ignore.not_found', false)) {
+                $this->tyto->dontSample();
+                $this->tyto->ingest->flush();
+
+                return;
+            }
+
             $this->tyto->stage(ExecutionStage::Sending);
         } catch (Throwable $e) {
             $this->tyto->report($e, handled: true);
